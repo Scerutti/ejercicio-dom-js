b74923b69de3241bcb645a7f6fe1c6f855477de1
Sebastián Cerutti <softsys95@gmail.com>
2023-05-06 20:02:53 -0300

    resolving the homework

--- homework/homework.js
+++ homework/homework.js
@@ -1,17 +1,13 @@
-// Obtener la referencia de todos los divs en una constante llamada "divs".
-//Escriba aquí su código:
+const divs = document.querySelectorAll("div");
 
+for (let i = 0; i < divs.length; i++) {
+    divs[i].innerText = "box " + (i + 1);
 
-/*
-Al obtener la referencia de los divs, usted podrá iterar estos mismos contenedores con métodos de javascript, cómo while o for.
-*/
-
-//Paso 1: recorra todos los divs del document.
-//Paso 2: En cada iteración usted tendrá que agregarle el texto "box" a cada una de las cajas, dejando como resultado "box 1", "box 2" y "box 3"
-//Pista: repasar la propiedad innerText
-//Paso 3: Con la propiedad "style" pintar cada uno de las cajas de un color: box 1 en rojo, box 2 en verde, y box 3 en azul.
-// Usted podría seleccionar cada elemento div, pero no tendría sentido si ya tenemos la referencia divs, aprovechar la iteración y realizar condicionales para obtener el resultado.
-//Escriba aquí su código:
-
-
-
+    if(i === 0){
+        divs[i].style.backgroundColor = "red";
+    } else if(i === 1){
+        divs[i].style.backgroundColor = "green";
+    }else if(i === 2) {
+        divs[i].style.backgroundColor = "blue";
+    }
+}
